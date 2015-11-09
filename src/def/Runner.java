95167aa43f25fca7852096a563284879d11d5c39
Ylvakiller <Rem.geuze@gmail.com>
2015-11-09 10:22:13 +0100

    Started the kalman2 assignment

---
+++ src/def/Runner.java
@@ -0,0 +1,14 @@
+package def;
+/**
+ * This class will run the kalman filter algorithm, this will do the interface
+ * @author Ylva
+ *
+ */
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
