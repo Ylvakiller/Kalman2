ce6e7e607be4ba9d4c7418d50ea7c3280db95fc1
Ylvakiller <Rem.geuze@gmail.com>
2015-11-20 13:13:39 +0100

    Proper start of the dataThread

--- src/lmpPackage/DataThread.java
+++ src/lmpPackage/DataThread.java
@@ -1,5 +1,32 @@
 package lmpPackage;
 
-public class DataThread {
+import java.util.ArrayList;
 
+public class DataThread {
+	/**
+	 * Will hold all the data
+	 */
+	private ArrayList<float[]> dataArray;
+	/**
+	 * Default constructor, added for convention sakes
+	 */
+	public DataThread(){
+		
+	}
+	
+	/**
+	 * Will add the given data to the end of the arrayList
+	 * @param pitch The pitch value to store
+	 * @param roll The roll value to store
+	 * @param yaw The yaw value to store
+	 */
+	public void addData(float pitch, float roll, float yaw){
+		float[] temp = new float[3];
+		temp[0] = pitch;
+		temp[1] = roll;
+		temp[2] = yaw;
+		dataArray.add(temp);
+	}
+	
+	
 }
