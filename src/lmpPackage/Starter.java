a27728ab8954052bffcb8fc6dd6d1004e930be68
Ylvakiller <Rem.geuze@gmail.com>
2015-11-26 15:55:05 +0100

    Printing the amount of values between both sensors

--- src/lmpPackage/Starter.java
+++ src/lmpPackage/Starter.java
@@ -17,7 +17,8 @@ public class Starter {
 		} catch (InterruptedException e) {//For some reason if I leave out this array it will send the second address twice
 			e.printStackTrace();//I have not been able to figure out why this happens and I will follow up as soon as I have an answer
 		}
-		System.out.println("Amount of values: " + sensor1.getAmount());
+		System.out.println("Amount of values filter 1: " + sensor1.getAmount());
+		System.out.println("Amount of values filter 1: " + sensor2.getAmount());
 		
 		System.out.println("runtime = " + (System.currentTimeMillis()-runtime));
 		System.exit(0);
