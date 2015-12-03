4f7f8e7b1a56f5dfce98ae13c0a1b76d7027b7d1
Ylvakiller <Rem.geuze@gmail.com>
2015-12-03 15:42:52 +0100

    Fourth time the charm.....

--- src/lmpPackage/Starter.java
+++ src/lmpPackage/Starter.java
@@ -8,18 +8,18 @@ public class Starter {
 		CommunicationThread com =  new CommunicationThread(1);
 		com.setPriority(Thread.MAX_PRIORITY);
 		SensorThread sensor1 = new SensorThread((byte) 0x53, (byte)0x69);
-		SensorThread sensor2 = new SensorThread((byte) 0x57, (byte)0x6d);
+		//SensorThread sensor2 = new SensorThread((byte) 0x57, (byte)0x6d);
 		com.start();
 		
 		sensor1.start();
-		sensor2.start();
+		//sensor2.start();
 		try {
 			Thread.sleep(10000);
 		} catch (InterruptedException e) {//For some reason if I leave out this array it will send the second address twice
 			e.printStackTrace();//I have not been able to figure out why this happens and I will follow up as soon as I have an answer
 		}
 		System.out.println("Amount of values filter 1: " + sensor1.getAmount());
-		System.out.println("Amount of values filter 2: " + sensor2.getAmount());
+		//System.out.println("Amount of values filter 2: " + sensor2.getAmount());
 		sensor1.getLast();
 		System.out.println("runtime = " + (System.currentTimeMillis()-runtime));
 		System.exit(0);
